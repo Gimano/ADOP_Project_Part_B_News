cd9d877cf9a721040814c2ad9311716f0815ed4c
Gimano <112472723+Gimano@users.noreply.github.com>
2023-01-09 18:59:04 +0100

    update

--- ADOP_Project_Part_B_News/Services/NewsService.cs
+++ ADOP_Project_Part_B_News/Services/NewsService.cs
@@ -1,4 +1,4 @@
-﻿//#define UseNewsApiSample  // Remove or undefine to use your own code to read live data
+﻿#define UseNewsApiSample  // Remove or undefine to use your own code to read live data
 
 using System;
 using System.Linq;
@@ -6,6 +6,7 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Json; 
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 using ADOP_Project_Part_B_News.Models;
 using ADOP_Project_Part_B_News.ModelsSampleData;
@@ -16,7 +17,15 @@ namespace ADOP_Project_Part_B_News.Services
     {
         HttpClient httpClient = new HttpClient();
         //Your API key
-        readonly string apiKey = "";
+        readonly string apiKey = "6b2990a1f70040498e70f10e83807d53";
+
+        FileNews fileNews = new FileNews();
+
+        public event EventHandler<string> NewsDataAvailable;
+        protected virtual void OnNewsDataAvailable(string message)
+        {
+            NewsDataAvailable?.Invoke(this, message);
+        }
 
         public NewsService()
         {
@@ -27,33 +36,92 @@ namespace ADOP_Project_Part_B_News.Services
 
         public async Task<News> GetNewsAsync(NewsCategory category)
         {
-#if UseNewsApiSample      
-            NewsApiData nd = await NewsApiSampleData.GetNewsApiSampleAsync(category);
+            News news = null;
+            var key = fileNews.ConvertToFileName(category, DateTime.Now.ToString("yyyy-MM-dd HH-mm"));
+
+            if (!File.Exists(key))
+            {
+#if UseNewsApiSample
+                NewsApiData nd = await NewsApiSampleData.GetNewsApiSampleAsync(category);
 
 #else
-            //https://newsapi.org/docs/endpoints/top-headlines
-            var uri = $"https://newsapi.org/v2/top-headlines?country=se&category={category}";
+                //https://newsapi.org/docs/endpoints/top-headlines
+                var uri = $"https://newsapi.org/v2/top-headlines?country=se&category={category}";
 
-            // make the http request
-            var httpRequest = new HttpRequestMessage(HttpMethod.Get, uri);
-            var response = await httpClient.SendAsync(httpRequest);
-            response.EnsureSuccessStatusCode();
+                // make the http request
+                var httpRequest = new HttpRequestMessage(HttpMethod.Get, uri);
+                var response = await httpClient.SendAsync(httpRequest);
+                response.EnsureSuccessStatusCode();
 
-            //Convert Json to Object
-            NewsApiData nd = await response.Content.ReadFromJsonAsync<NewsApiData>();
+                //Convert Json to Object
+                NewsApiData nd = await response.Content.ReadFromJsonAsync<NewsApiData>();
 #endif
-            var news = new News()
-            {
-                Category = category,
-                Articles = nd.Articles.Select(ndi => new NewsItem()
+                news = new News()
                 {
-                    DateTime = ndi.PublishedAt,
-                    Title = ndi.Title,
-                    Url = ndi.Url,
-                    UrlToImage = ndi.UrlToImage
-                }).ToList()
-            };
+                    Category = category,
+                    Articles = nd.Articles.Select(ndi => new NewsItem()
+                    {
+                        DateTime = ndi.PublishedAt,
+                        Title = ndi.Title,
+                        Url = ndi.Url,
+                        UrlToImage = ndi.UrlToImage
+                    }).ToList()
+                };
+                return news;
+            }
+            news = await FileNews.DeserializeCacheAsync(news, key);
+            OnNewsDataAvailable($"XML Cached News for category {category} is available");
             return news;
+        }
+    }
+
+    public class FileNews
+    {
+        public string ConvertToFileName(NewsCategory n, string dt)
+        {
+            return Fname($"Cached_News_for_{n}_{dt}.xml");
+        }
+        static string Fname(string name)
+        {
+            var documentPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            documentPath = Path.Combine(documentPath, "NewsService");
+            if (!Directory.Exists(documentPath)) Directory.CreateDirectory(documentPath);
+            return Path.Combine(documentPath, name);
+        }
+        static public Task SerializeCacheAsync(News news, string name)
+        {
+            return Task.Run(() => SerializeCache(news, name));
+        }
+
+        static public void SerializeCache(News news, string name)
+        {
+            object _lock = new object();
+            lock (_lock)
+            {
+                XmlSerializer xs = new XmlSerializer(typeof(News));
+                using (Stream s = File.Create(name))
+                {
+                    xs.Serialize(s, news);
+                }
+            }
+        }
+        static public Task<News> DeserializeCacheAsync(News news, string name)
+        {
+            return Task.Run(() => DeserializeCache(news, name));
+        }
+
+        static public News DeserializeCache(News news, string name)
+        {
+            object _lock = new object();
+            lock (_lock)
+            {
+                XmlSerializer xs = new XmlSerializer(typeof(News));
+                using (Stream s = File.OpenRead(name))
+                {
+                    News returnNews = (News)xs.Deserialize(s);
+                    return returnNews;
+                }
+            }
         }
     }
 }
